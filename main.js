f7d0e2d6844e815941deba6e6b741e6334b9720c
Neil Alaan <108949710+sininen-blue@users.noreply.github.com>
2025-11-10 04:39:12 +0000

    fly

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { OBJLoader } from "three/addons/loaders/OBJLoader.js";
 import { MTLLoader } from "three/addons/loaders/MTLLoader.js";
+import { FlyControls } from "three/addons/controls/FlyControls.js";
+import { FirstPersonControls } from "three/addons/controls/FirstPersonControls.js";
 
 let coin;
 
@@ -12,15 +14,17 @@ function main() {
   const fov = 75;
   const aspect = 2;
   const near = 0.1;
-  const far = 5;
+  const far = 1000;
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
   camera.position.z = 2;
+  camera.position.y = 2;
 
   const scene = new THREE.Scene();
 
-  const controls = new OrbitControls(camera, canvas);
-  controls.target.set(0, 0, 0);
-  controls.update();
+  const controls = new FlyControls(camera, renderer.domElement);
+  controls.movementSpeed = 5;
+  controls.rollSpeed = 1;
+  controls.dragToLook = true;
 
   {
     const color = 0xffffff;
@@ -28,6 +32,30 @@ function main() {
     const light = new THREE.DirectionalLight(color, intensity);
     light.position.set(-1, 2, 4);
     scene.add(light);
+  }
+
+  {
+    const planeSize = 40;
+
+    const loader = new THREE.TextureLoader();
+    const texture = loader.load(
+      "https://threejs.org/manual/examples/resources/images/checker.png"
+    );
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.magFilter = THREE.NearestFilter;
+    texture.colorSpace = THREE.SRGBColorSpace;
+    const repeats = planeSize / 2;
+    texture.repeat.set(repeats, repeats);
+
+    const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
+    const planeMat = new THREE.MeshPhongMaterial({
+      map: texture,
+      side: THREE.DoubleSide,
+    });
+    const mesh = new THREE.Mesh(planeGeo, planeMat);
+    mesh.rotation.x = Math.PI * -0.5;
+    scene.add(mesh);
   }
 
   {
@@ -51,14 +79,15 @@ function main() {
       });
     });
   }
+  scene.add(new THREE.AmbientLight(0xffffff, 0.5));
   const keys = {};
   window.addEventListener("keydown", (e) => (keys[e.code] = true));
   window.addEventListener("keyup", (e) => (keys[e.code] = false));
+
+  const clock = new THREE.Clock();
+
   function render(time) {
-    if (keys["KeyW"]) coin.position.z -= 0.1;
-    if (keys["KeyS"]) coin.position.z += 0.1;
-    if (keys["KeyD"]) coin.position.x += 0.1;
-    if (keys["KeyA"]) coin.position.x -= 0.1;
+    controls.update(clock.getDelta());
     renderer.render(scene, camera);
 
     requestAnimationFrame(render);
